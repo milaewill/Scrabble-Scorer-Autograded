ccfde84044b358ae94138a54e3275bc9d250ea02
Mila Williams <milaewill@Milas-MBP.lan>
2023-10-05 13:08:15 -0500

    task2

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,27 +33,70 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   let word = input.question("Let's play some scrabble! Enter a word: ");
+   let word = input.question("Let's play some scrabble! \n\nEnter a word: ");
    return word;
 };
 
-let simpleScorer;
+let simpleScorer = function (word) {
+   word = word.toUpperCase();
+   return word.length;
+};
 
-let vowelBonusScorer;
+let vowelBonusScorer = function (word) {
+   word = word.toUpperCase();
+   const vowels = ['A', 'E', 'I', 'O', 'U'];
+   let letterPoints = 0;
+   for (let i = 0; i < word.length; i++) {
+      if (vowels.includes(word[i])) {
+         letterPoints += 3
+      } else {
+         letterPoints += 1
+      }
+   }
+   return letterPoints;
+};
 
 let scrabbleScorer;
 
-const scoringAlgorithms = [];
 
-function scorerPrompt() { }
+//Scoring Objects
+let simpleScorerObject = {
+   name: "Simple Score",
+   description: "Each letter is worth 1 point.",
+   scoringFunction: simpleScorer
+}
+let vowelBonusScorerObject = {
+   name: "Bonus Vowels",
+   description: "Vowels are 3 pts, consonants are 1 pt.",
+   scoringFunction: vowelBonusScorer
+}
+let scrabbleScorerObject = {
+   name: "Scrabble",
+   description: "The traditional scoring algorithm.",
+   scoringFunction: scrabbleScorer
+}
+
+const scoringAlgorithms = [simpleScorerObject, vowelBonusScorerObject, scrabbleScorerObject];
+
+function scorerPrompt() {
+   let choice = input.question("Which scoring algorithm would you like to use? \n\n0 - Simple: one point per character \n1 - Vowel Bonus: Vowels are worth 3 points \n2 - Scrabble: Uses scrabble pint system \nEnter 0, 1, or 2: \n")
+   while (choice < 0 || choice > 2 || isNaN(choice)) {
+      choice = input.question("Choose Again- \nEnter 0, 1, or 2: ")
+   }
+   let userChoice = scoringAlgorithms[choice];
+   return userChoice;
+}
 
 function transform() { };
 
 let newPointStructure;
 
-function runProgram() {
+function runProgram() {      //order that our page is running
    let word = initialPrompt();
-   console.log(oldScrabbleScorer(word))
+   let scoringSystem = scorerPrompt()
+   let userChoice = scoringSystem.scoringFunction(word)
+   console.log(`Score for '${word}': ${userChoice}`)
+   oldScrabbleScorer(word)
 
 }
 
